459f4c23a978b6c5ff4ceedcf06e7c088c4de153
Merv Barrett <mervb1@gmail.com>
2017-02-15 11:36:32 +0800

    Building Sort Options

--- sort/sort-building-area.php
+++ sort/sort-building-area.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Set the default sort to building area
+ * Set the default sort to building area and add building sort options
  *
  */
 
@@ -24,3 +24,37 @@ function my_epl_listing_default_sort_building( $query ) {
 	}
 }
 add_action( 'pre_get_posts', 'my_epl_listing_default_sort_building' , 1  );
+
+// Add Building sort options to sorter
+function my_epl_add_sorting_options_building_callback( $sorters ) {
+
+	$sorters[] = array(
+		'id'		=>	'building_asc',
+		'label'		=>	__('Building : High','easy-property-listings' ),
+		'type'		=>	'meta',
+		'key'		=>	'property_building_area',
+		'order'		=>	'DESC',
+		'orderby'	=>	'meta_value_num',
+	);
+	$sorters[] = array(
+		'id'		=>	'building_desc',
+		'label'		=>	__('Building : Low','easy-property-listings' ),
+		'type'		=>	'meta',
+		'key'		=>	'property_building_area',
+		'order'		=>	'ASC',
+		'orderby'	=>	'meta_value_num',
+	);
+
+	return $sorters;
+}
+add_filter( 'epl_sorting_options' , 'my_epl_add_sorting_options_building_callback');
+
+// Display the Building Size on the Loop Template
+function my_output_building_size_callback() {
+	global $property;
+
+	$building_size = $property->get_property_building_area_value();
+
+	echo '<ul>' . $building_size . '</ul>';
+}
+add_action( 'epl_the_excerpt' , 'my_output_building_size_callback' , 16 );
