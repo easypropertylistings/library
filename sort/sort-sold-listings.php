ade6568993889b99e1063df27b8cdba7a79be1a7
Merv Barrett <mervb1@gmail.com>
2017-03-10 16:31:11 +0800

    Added Sorting by Sold Date function

---
+++ sort/sort-sold-listings.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Sort Sold Listings by Page slug
+ *
+ */
+
+function my_epl_sort_recent_sales( $query ) {
+
+	// Do nothing if page is not recent sales page
+	if ( is_admin() || $query->is_main_query() || is_search() || !is_page('recent-sales') ) // Adjust the page slug
+		return;
+
+	if( !in_array('property',(array) $query->query_vars['post_type']))
+		return;
+
+	// Do nothing if Easy Property Listings is not active
+	if ( ! function_exists( 'epl_all_post_types' ) )
+		return;
+
+	$meta_query = array(
+
+		'property_sold_date'	=>
+			array(
+				'key'		=> 'property_sold_date',
+				'value'		=> '',
+				'type'		=>	'DATE',
+				'compare'	=> '!=',
+			),
+	);
+
+	$query->set( 'posts_per_page' , 99 ); // Adjust the quantity
+	$query->set('meta_query',$meta_query);
+	$query->set( 'orderby',
+		array(
+			'property_sold_date' 	=> 'DESC'
+		)
+	);
+}
+add_action( 'pre_get_posts', 'tpe_sort_recent_sales' , 1  );
