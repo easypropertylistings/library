b4f897788489ebddda3ec9746fad45389d639c1d
Merv Barrett <mervb1@gmail.com>
2017-04-03 12:13:41 +0800

    Added search field order

---
+++ search/search-field-order.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Alter the order of the search fields
+ *
+ */
+
+function my_epl_custom_search_field_order($fields) {
+
+	foreach( $fields as &$field ) {
+
+		// change order of bedrooms
+		if( $field['key'] == 'property_bedrooms' ){
+			$field['order'] = 200;
+		}
+
+		// change order of bedrooms
+		if( $field['key'] == 'property_bathrooms' ){
+			$field['order'] = 220;
+		}
+	}
+
+	return $fields;
+}
+add_filter('epl_search_widget_fields_frontend','my_epl_custom_search_field_order');
