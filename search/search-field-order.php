bfdca1eb36a6a0d5875a6cbf2485e0916f4b4a34
Merv Barrett <mervb1@gmail.com>
2017-04-03 12:22:39 +0800

    Order Altered

--- search/search-field-order.php
+++ search/search-field-order.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Alter the order of the search fields
+ * Default order is stored in https://github.com/easypropertylistings/Easy-Property-Listings/blob/master/lib/widgets/widget-functions.php
  *
  */
 
@@ -8,14 +9,18 @@ function my_epl_custom_search_field_order($fields) {
 
 	foreach($fields as &$field) {
 
-		// change order of bedrooms
-		if( $field['key'] == 'property_bedrooms'){
-			$field['order'] = 200;
+		// change order of min bedrooms
+		if( $field['meta_key'] == 'property_bedrooms_min'){
+			$field['order'] = 1;
+		}
+		// change order of max bedrooms
+		if( $field['meta_key'] == 'property_bedrooms_max'){
+			$field['order'] = 2;
 		}
 
 		// change order of bathrooms
-		if( $field['key'] == 'property_bathrooms'){
-			$field['order'] = 220;
+		if( $field['meta_key'] == 'property_bathrooms'){
+			$field['order'] = 5;
 		}
 	}
 
