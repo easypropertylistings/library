b69d44cc4f901fa2d723d431e2c04486f007c8c0
wpdevstudio <prince@wpdevstudio.com>
2017-08-08 14:27:51 +0530

    Sample of how to add custom tax search in EPL

---
+++ search/custom-tax-search.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Add custom tax search in EPL
+ */
+
+/**
+ * Add field in frontend form
+ */
+function my_epl_search_widget_fields_frontend($fields) {
+	
+	// assuming tax = property_custom_tax
+
+	$custom_tax	= get_terms('property_custom_tax',array('hide_empty'	=> true);
+	$arr = array();
+	foreach($custom_tax as $custom_tax_single) {
+		$arr[$custom_tax_single->term_id] = $custom_tax_single->name;
+	}
+
+	$fields[] = array(
+		'key'			=>	'search_custom_tax',
+		'meta_key'		=>	'property_custom_tax',
+		'label'			=>	'Custom Tax',
+		'type'			=>	'select',
+		'option_filter'	=>	'custom_tax',
+		'options'		=>	$arr,
+		'query'			=>	array('query'	=>	'tax'),
+		'class'			=>	'epl-search-row-full',
+		'order'			=>	40
+	);
+
+	return $fields;
+}
+add_filter('epl_search_widget_fields_frontend','my_epl_search_widget_fields_frontend');
+
+/**
+ * Add field in backend / widget
+ */
+
+function my_epl_search_widget_fields($fields) {
+	
+	// assuming tax = property_custom_tax
+
+	$fields[] = array(
+		'key'			=>	'search_custom_tax',
+		'label'			=>	'Custom Tax',
+		'default'		=>	'on',
+		'type'			=>	'checkbox',
+	);
+
+	return $fields;
+}
+add_filter('epl_search_widget_fields','my_epl_search_widget_fields');
