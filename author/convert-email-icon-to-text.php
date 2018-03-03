4ed37711115c61cde25ce8129506b451cd18a8f3
Merv Barrett <mervb1@gmail.com>
2018-03-03 12:12:23 +0800

    Corrected for secondary author email

--- author/convert-email-icon-to-text.php
+++ author/convert-email-icon-to-text.php
@@ -5,12 +5,10 @@
  *
  * @see http://docs.easypropertylistings.com.au/source-class-EPL_Author_Meta.html#81
  */
-function rec_epl_author_icon_email_text_callback( $html = '') {
-
-	global $epl_author;
+function rec_epl_author_icon_email_text_callback( $html = '' , $epl_author ) {
 
 	$html = '<a class="epl-author-email" href="mailto:' . $epl_author->email . '" title="'.__('Contact', 'easy-property-listings' ).' '.$epl_author->name.' '.__('by Email', 'easy-property-listings' ).'">' . $epl_author->email . '</a>';
 
 	return $html;
 }
-add_filter( 'epl_author_icon_email' , 'rec_epl_author_icon_email_text_callback' , 1 );
+add_filter( 'epl_author_icon_email' , 'rec_epl_author_icon_email_text_callback' , 1 , 2);
