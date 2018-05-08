fb46978f2fde0faa64a25f5e79183f210c5b5f98
Merv Barrett <mervb1@gmail.com>
2018-05-08 09:43:04 +0800

    Filter to display the price when a listing is under offer

---
+++ listing/display-price-when-under-offer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Filter to display the price when a listing is under offer
+ *
+ */
+
+function my_epl_get_price( $price ) {
+
+	global $property;
+
+	if ( 'yes' == $property->get_property_meta('property_under_offer') && 'sold' != $property->get_property_meta('property_status') ) {
+		$price = $property->get_property_price_display();
+		$price = '<span class="page-price under-offer-status">'.$property->label_under_offer .  ' ' . $price .    '</span>';
+	}
+	return $price;
+}
+add_filter( 'epl_get_price' , 'my_epl_get_price' );
