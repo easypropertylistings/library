0748562a6088425a65229d2d398fde47a6885d12
wpdevstudio <prince@wpdevstudio.com>
2019-01-15 12:18:51 +0530

    Location multiselect for EPL search

---
+++ search/location-multiselect.php
@@ -0,0 +1,12 @@
+<?php
+function epl_filter_search_widget_fields_frontend($fields) {
+
+ 	foreach($fields as &$field) {
+ 		if($field['key'] == 'search_location') {
+ 			$field['type'] 		= 	'multiple_select';
+ 			break;
+ 		}
+ 	}
+ 	return $fields;
+ }
+add_filter('epl_search_widget_fields_frontend','epl_filter_search_widget_fields_frontend');
