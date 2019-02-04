af07445bc54945ae42dc66ddb75efce1adae53e7
Merv Barrett <mervb1@gmail.com>
2019-02-04 12:57:57 +0100

    description update

--- listing/custom-map-marker.js
+++ listing/custom-map-marker.js
@@ -1,6 +1,6 @@
-/* Custom Map Marker icon for default Google Map JS */
+/* Custom Map Marker icon for default Google Map PNG implementation and disable default SVG */
 var marker = new google.maps.Marker({
-    position: eplmapcord,
-    map: epldefaultmap,
-        icon : 'https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png'
-  });
+	position: eplmapcord,
+	map: epldefaultmap,
+	icon: 'https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png'
+});
