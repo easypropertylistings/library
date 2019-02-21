068aea6153fe7613f50a9bd872f354ea201559ae
Merv Barrett <mervb1@gmail.com>
2019-02-21 14:13:05 +0100

    wrapped for epl single

--- listing/custom-map-marker.php
+++ listing/custom-map-marker.php
@@ -1,20 +1,20 @@
 <?php
+/** Custom map marker script **/
 function my_custom_marker_scripts() {
-?>
-	<script type="text/javascript">
-		jQuery(document).ready( function($) {
-			setTimeout(function() {
-				epldefaultmarker.setMap(null);
-				epldefaultmarker = new google.maps.Marker({
-					position: epldefaultmap.getCenter(),
-					map: epldefaultmap,
-					icon : 'http://someURL.com.au/wp-content/themes/Divi-child/img/icon-map.png'
-				});
-			},
-			0);
 
-		});
-	</script>
-<?php
+    if ( is_epl_post_single() ) { ?>
+        <script type="text/javascript">
+    		jQuery(document).ready( function($) {
+    			epldefaultmarker.setMap(null);
+    			epldefaultmarker = new google.maps.Marker({
+    				position: epldefaultmap.getCenter(),
+    				map: epldefaultmap,
+    				icon : 'http://someURL.com.au/wp-content/themes/Divi-child/img/icon-map.png'
+    			});
+
+    		});
+    	</script>
+    <?php
+    }
 }
 add_action('wp_footer','my_custom_marker_scripts');
