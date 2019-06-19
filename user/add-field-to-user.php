d049716a552963c882891cb597258e3e3e9b4cbb
Merv Barrett <mervb1@gmail.com>
2019-06-19 09:34:03 +0800

    Add field to the User Profile in the EPL Section

---
+++ user/add-field-to-user.php
@@ -0,0 +1,13 @@
+<?php
+// Add field to the User Profile in the EPL Section
+function my_epl_add_author_box_fields($fields) {
+	$fields[] = array(
+				'name'		=>	'some_unique_name',
+				'label'		=>	__('My label', 'easy-property-listings' ),
+				'description'	=>	__('My description of the field', 'easy-property-listings' ),
+				'class'		=>	'regular-text',
+				'type'		=>	'text',
+			);
+	return $fields;
+}
+add_filter('epl_custom_user_profile_fields','my_epl_add_author_box_fields')
