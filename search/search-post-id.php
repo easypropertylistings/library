a520049085944c627e160d464c5e11b3f2e7f833
Merv Barrett <mervb1@gmail.com>
2019-07-11 12:29:16 +0800

    Update search-post-id.php

--- search/search-post-id.php
+++ search/search-post-id.php
@@ -25,16 +25,33 @@ add_filter('epl_search_widget_fields', 'my_epl_search_widget_fields_post_id');
  *
  **/
 function my_epl_add_search_field_post_id($fields) {
+
  	$fields[] = array(
-			'key'			=>	'search_id',
-			'meta_key'		=>	'post_id',
+			'key'			=>	'search_post_id',
+			'meta_key'		=>	'listing_id',
 			'label'			=>	__('Search by Property ID', 'easy-property-listings'),
 			'placeholder'		=>	__('Search ID', 'easy-property-listings'),
 			'type'			=>	'text',
 			'class'			=>	'epl-search-row-full',
-			'query'			=>	array('query'	=>	'meta' , 'key'	=>	'property_unique_id'),
+			'query'			=>	array('query'	=>	'special'),
 			'order'			=>	25
 		);
  	return $fields;
 }
 add_filter('epl_search_widget_fields_frontend','my_epl_add_search_field_post_id');
+
+/**
+ * Enable search by post ID
+ */
+
+function my_epl_search_special_query($query,$field,$value) {
+
+
+	if( $field['meta_key'] == 'listing_id' ){
+		$value = array_map( 'trim', explode( ',', $value ) );
+		$query->set('post__in', $value );
+
+	}
+	return $query;
+}
+add_filter('epl_search_special_query','my_epl_search_special_query',10,3);
