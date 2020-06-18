c21ee86e32fef721417fbb4f56e75099d42434a4
mervb1@gmail.com <mervb1@gmail.com>
2020-06-18 13:07:09 +0800

    stickers updated

--- listing/stickers-for-property-status.php
+++ listing/stickers-for-property-status.php
@@ -1,47 +1,34 @@
 <?php
 /**
- * Add custom sticker for property category.
- *
- * @requires EPL 3.4.27
+ * Add custom sticker for property status.
  *
  */
 
 /**
- * Add a sticker for property category
+ * Add a sticker for for sale / for rent.
  */
-function my_add_epl_sticker( $stickers_array ) {
-	// loop $stickers_array to change existing stickers
-	// or add a new one
-	$stickers_array['category'] = array(
-		'conditions'			=>	array(
-			'property_category'			=>	'',
-		),
-		'compare'			=>	'!=',
-		'type'				=>	epl_get_core_post_types(),
-		'label'				=>	get_property_meta( 'property_category' ),
-		'before'			=>	'',
-		'after'				=>	'',
-		'class'				=>	'listing-category'
-	);
-	return $stickers_array;
-}
-add_filter( 'epl_available_stickers', 'my_add_epl_sticker' );
+function my_epl_property_stickers() {
 
-/**
- * filter to change stickers which will render for current listing
- */
-function my_customise_epl_sticker( $stickers_array ) {
-	// dont render for sale stickers for property
-	if( isset( $stickers_array[ 'current_sales'] ) )
-		unset( $stickers_array[ 'current_sales'] );
-	// change label for home open
-	if( isset( $stickers_array[ 'home_open'] ) ) {
-		$stickers_array[ 'home_open']['label'] = __( 'Live Inspections', 'theme' );
+	global $property;
+
+	if ( 'property' === $property->post_type || 'land' === $property->post_type || 'rural' === $property->post_type || 'business' === $property->post_type ) {
+		$price_sticker = '';
+		if ( 'current' === $property->get_property_meta( 'property_status' ) ) {
+			$price_sticker .= '<span class="status-sticker for-sale">For Sale</span>';
+		}
+		if ( 'yes' === $property->get_property_meta( 'property_under_offer' ) && 'sold' !== $property->get_property_meta( 'property_status' ) ) {
+			$price_sticker  = '';
+		}
 	}
-	// add an icon before sold label
-	if( isset( $stickers_array[ 'sold'] ) ) {
-		$stickers_array[ 'sold']['before'] = '<i class="fa fa-key"></i>';
+
+	if ( 'rental' === $property->post_type ) {
+		$price_sticker = '';
+		if ( 'current' === $property->get_property_meta( 'property_status' ) ) {
+			$price_sticker .= '<span class="status-sticker for-rent">For Rent</span>';
+		}
 	}
-	return $stickers_array;
+
+	echo $price_sticker;
+
 }
-add_filter( 'epl_stickers_array', 'my_customise_epl_sticker' );
+add_action( 'epl_property_stickers', 'my_epl_property_stickers' );
