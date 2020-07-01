5cc5b5bda2909b6039f7f7d528a61ff7e5c26a23
mervb1@gmail.com <mervb1@gmail.com>
2020-07-01 11:18:59 +0800

    Update land-in-house-category.php

--- search/land-in-house-category.php
+++ search/land-in-house-category.php
@@ -1,55 +1,53 @@
 <?php
-// General Functions
+/**
+ * Add the Land post type as a search House Category.
+ */
 
-//Add Land option in Property Category
-function epl_search_field_customizations($fields) {
-  foreach($fields as $field_key   =>  &$field) {
-  	  if($field['meta_key'] == 'property_category') {
-         $field['options']['Land'] = __('Land','easy-property-listings');
-        }       
-  }
-  
-  return $fields;
+// Add Land option in Property Category.
+function my_epl_search_add_land( $fields ) {
+	foreach($fields as $field_key   =>  &$field) {
+		if($field['meta_key'] == 'property_category') {
+			$field['options']['Land'] = __('Land','easy-property-listings');
+		}
+	}
+	return $fields;
 }
-add_filter('epl_search_widget_fields_frontend','epl_search_field_customizations');
+add_filter( 'epl_search_widget_fields_frontend', 'my_epl_search_add_land' );
 
-//Add script to select Land Posttype on selecting Category Land
-function rec_search_script() { ?>
+// Add script to select Land Post type on selecting Category Land.
+function my_epl_search_land_script() { ?>
 	<script type="text/javascript">
 		jQuery(document).ready(function($) {
-
-		    //On select change
+			// On select change.
 			$('.epl-search-forms-wrapper #property_category').on('change', function(e) {
 		        if( $(this).find(':selected').val() == 'Land' ) {
-		            $(this).closest('form').find('#post_type').val('land');
+		        	$(this).closest('form').find('#post_type').val('land');
 		        } else {
 		        	$(this).closest('form').find('#post_type').val('property');
 		        }
-
 		    });
 		});
 	</script>
-<?php }
-add_action('wp_footer','rec_search_script');
-
-
-//Unset Property category if Land is selected
-function rec_unset_property_category($query) {
-   $meta_query = $query->get('meta_query');
-    // Do nothing if in dashboard or not an archive page
-    
-    if (epl_is_search() && $_GET['property_category'] == 'Land') {
-        
-        if( !empty( $meta_query ) ) :
-        foreach($meta_query as $index => &$metakey) {
-            if($metakey['key'] == 'property_category') {
-                
-               unset($meta_query[$index]);
-            }
-        }
-        endif;
-        
-        $query->set('meta_query',$meta_query);
-    }
+<?php
 }
-add_filter('pre_get_posts','rec_unset_property_category');
+add_action( 'wp_footer', 'my_epl_search_land_script' );
+
+// Unset Property category if Land is selected.
+function my_epl_search_unset_property_category($query) {
+	$meta_query = $query->get('meta_query');
+
+	// Do nothing if in dashboard or not an archive page.
+	if (epl_is_search() && $_GET['property_category'] == 'Land') {
+
+		if( !empty( $meta_query ) ) :
+		foreach($meta_query as $index => &$metakey) {
+			if($metakey['key'] == 'property_category') {
+				unset($meta_query[$index]);
+			}
+		}
+		endif;
+
+		$query->set('meta_query',$meta_query);
+	}
+}
+add_filter( 'pre_get_posts', 'my_epl_search_unset_property_category' );
