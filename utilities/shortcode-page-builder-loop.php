620c12accb87238a59406f893df01cdb3c02714f
mervb1@gmail.com <mervb1@gmail.com>
2020-07-15 12:18:23 +0800

    Load the Easy Property Listings Loop using a shortcode. For use in Page Buiders

---
+++ utilities/shortcode-page-builder-loop.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Load the Easy Property Listings Loop using a shortcode. For use in Page Buiders
+ */
+
+// Page Builder Archive / Search Results Loop
+function epl_search_results_loop_callback() {
+
+	ob_start(); ?>
+
+	<?php if(have_posts()) : ?>
+
+		<?php do_action( 'epl_property_loop_start' ); ?>
+
+		<?php while(have_posts()) : the_post(); ?>
+			<?php do_action( 'epl_property_blog' ); ?>
+		<?php endwhile; ?>
+
+		<?php do_action( 'epl_property_loop_end' ); ?>
+
+		<div class="loop-footer">
+			<!-- Previous/Next page navigation -->
+			<div class="loop-utility clearfix">
+				<?php do_action('epl_pagination'); ?>
+			</div>
+		</div>
+
+		<?php else : ?>
+
+			<?php do_action( 'epl_property_search_not_found' ); ?>
+
+		<?php endif; ?>
+
+	<?php
+		return ob_get_clean();
+
+}
+add_shortcode( 'epl_search_results_loop', 'epl_search_results_loop_callback' );
