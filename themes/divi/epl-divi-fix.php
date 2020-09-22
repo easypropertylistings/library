ac20f5bbf11fbe14872de85de7aa1e9576b0ce17
mervb1@gmail.com <mervb1@gmail.com>
2020-09-22 10:35:37 +0800

    Using head

--- themes/divi/epl-divi-fix.php
+++ themes/divi/epl-divi-fix.php
@@ -10,6 +10,6 @@
 
 // Divi Missing Hooks for SVG Icons
 if ( function_exists( 'epl_load_svg_listing_icons_head' ) ) {
-	add_action( 'et_after_main_content', 'epl_load_svg_listing_icons_head', 900 );
-	add_action( 'et_after_main_content', 'epl_load_svg_social_icons_head', 900 );
+	add_action( 'et_before_main_content', 'epl_load_svg_listing_icons_head', 900 );
+	add_action( 'et_before_main_content', 'epl_load_svg_social_icons_head', 900 );
 }
