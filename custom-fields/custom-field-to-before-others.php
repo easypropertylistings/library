d3433655febe48c91c1e1541b00fc0fbe8539d4a
mervb1@gmail.com <mervb1@gmail.com>
2020-10-12 18:59:09 +0800

    Fixed as it was overriding

--- custom-fields/custom-field-to-before-others.php
+++ custom-fields/custom-field-to-before-others.php
@@ -7,28 +7,27 @@
  * Add select box field to the land details
  * @uses EPL Filter epl_meta_groups_{group_id}
  */
-function my_epl_frontage_field( $group ) {
-
+function my_epl_add_frontage_field( $group ) {
 	$new_fields = array(
-		array(
+		'property_frontage'  =>   array(
 			'name'      => 'property_frontage',
 			'label'     => __( 'Frontage', 'easy-property-listings' ),
 			'type'      => 'decimal',
 			'maxlength' => '50',
 			'width'     => '2-3',
 		),
-		array(
+		'property_frontage_unit'    =>   array(
 			'name'  => 'property_frontage_unit',
 			'label' => __( 'Frontage Unit', 'easy-property-listings' ),
 			'type'  => 'select',
 			'opts'  => array(
-			'metre' => __( 'Metre', 'easy-property-listings' ),
-		),
+				'metre' => __( 'Metre', 'easy-property-listings' ),
+			),
 			'width' => '3',
 		)
 	);
-
 	$group['fields'] = $new_fields + $group['fields'];
+
 	return $group;
 }
-add_filter( 'epl_meta_groups_land_details', 'my_epl_frontage_field' );
+add_filter( 'epl_meta_groups_land_details', 'my_epl_add_frontage_field' );
