520a54161e691aba325677414ab97969282b3508
mervb1@gmail.com <mervb1@gmail.com>
2020-10-22 13:46:46 +0800

    Create import-delete-attached-images.php

---
+++ importer/import-delete-attached-images.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Delete attached images of a specified post type
+ *
+ * @link: https://toolset.com/forums/topic/proper-post-attachment-management-via-cred/
+ */
+
+
+
+
+function delete_post_children($post_id) {
+	global $wpdb;
+
+	if ( get_post_type( $post_id ) == 'property' ) {
+		$ids = $wpdb->get_col("SELECT ID FROM {$wpdb->posts} WHERE post_parent = $post_id AND post_type = 'attachment'");
+		foreach ( $ids as $id ) {
+			wp_delete_attachment( $id, true );
+		}
+		if( has_post_thumbnail( $post_id ) ) {
+			$tn_id = get_post_thumbnail_id( $post_id );
+			wp_delete_attachment( $tn_id, true );
+		}
+	}
+}
+add_action('before_delete_post', 'delete_post_children');
