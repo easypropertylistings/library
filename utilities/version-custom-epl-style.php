ace6eb4b4e01acff4b7568fcadbb2be2985d32e7
Merv Barrett <merv@realestateconnected.com.au>
2020-10-27 13:04:03 +0800

    Update version-custom-epl-style.php

--- utilities/version-custom-epl-style.php
+++ utilities/version-custom-epl-style.php
@@ -12,6 +12,5 @@ function my_epl_versioned_stylesheet() {
 		$version = filemtime( get_stylesheet_directory() . '/easypropertylistings/style-versioned.css');
 		wp_enqueue_style( 'epl-versioned-style', get_stylesheet_directory_uri() . '/easypropertylistings/style-versioned.css', false, $version );
 	}
-
 }
-add_action( 'wp_enqueue_scripts', 'iacbr_custom_scripts' );
+add_action( 'wp_enqueue_scripts', 'my_epl_versioned_stylesheet' );
