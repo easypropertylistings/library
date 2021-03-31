60e68634fe545e087f570586877d8a9dc11bcac1
Merv Barrett <merv@realestateconnected.com.au>
2021-03-31 11:14:29 +0800

    Update output-sold-date.php

--- custom-fields/output-sold-date.php
+++ custom-fields/output-sold-date.php
@@ -1,15 +1,19 @@
 <?php
-/*
- * Output the sold date next to the price field
+/**
+ * Output the sold date next to the price field.
+ *
+ * PHP Date/Time Format: https://www.php.net/manual/en/datetime.format.php
  */
-
 function my_sold_date_test() {
 
     echo '<p>Sold Date Test</p>';
 
     $sale_date = get_property_meta('property_sold_date');
 
-    echo $sale_date;
+    $php_date_format = 'j F, Y';
+    $formatted_date  = date( $php_date_format, strtotime( $sale_date ) );
+
+    echo $formatted_date;
 
 }
 add_action( 'epl_property_price' , 'my_sold_date_test' );
