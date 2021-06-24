888a3aac1e1bba2444e1b88bc92cafa889be996f
mervb1@gmail.com <mervb1@gmail.com>
2021-06-24 13:57:51 +0800

    Update add-to-array.php

--- array/add-to-array.php
+++ array/add-to-array.php
@@ -12,7 +12,7 @@
  * https://github.com/easypropertylistings/Easy-Property-Listings/blob/master/lib/widgets/class-epl-widget-recent-property.php
  *
  */
-function my_add_to_array( $existing_array ) {
+function my_add_to_end_of_array( $existing_array ) {
 
 	$existing_array['coming_soon'] = 'Coming Soon'; // Will add this to the END of the items in the array.
 
@@ -27,13 +27,32 @@ function my_add_to_array( $existing_array ) {
  * https://github.com/easypropertylistings/Easy-Property-Listings/blob/master/lib/widgets/class-epl-widget-recent-property.php
  *
  */
-function my_add_to_array( $existing_array ) {
+function my_add_to_beginning_of_array( $existing_array ) {
 
 	$new_fields = [
 		'coming_soon' => 'Coming Soon'
 	];
 
 	$existing_array = array_merge( $new_fields, $existing_array );
+
+	return $existing_array;
+
+}
+
+/**
+ * Add a new item to the beginning of an existing array. (Another way)
+ *
+ * Using epl_widget_listing_property_status asn an example in
+ * https://github.com/easypropertylistings/Easy-Property-Listings/blob/master/lib/widgets/class-epl-widget-recent-property.php
+ *
+ */
+function my_add_to_beginning_of_array_another_way( $existing_array ) {
+
+	$new_fields = [
+		'coming_soon' => 'Coming Soon'
+	];
+
+	$existing_array = $new_fields + $existing_array;
 
 	return $existing_array;
 
