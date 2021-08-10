f995fe61f2b9b562fac9ba3aaf0e2736aa4e1426
mervb1@gmail.com <mervb1@gmail.com>
2021-08-10 11:49:11 +0800

    Update search-restrict-results-time-duration.php

---
+++ search/search-restrict-results-time-duration.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Restrict the search results by days back.
+ *
+ */
+function my_epl_mod_date_custom_search_processing($meta_query) {
+
+	$days_back = 8 * 7; // week * days.
+
+	$dates = [
+		date ( 'Y-m-d', strtotime( '- '. $days_back. ' days' ) ),
+		date ( 'Y-m-d' ),
+	];
+
+	$mod_date_query = array(
+		array(
+			'key'     => 'property_mod_date',
+			'type'    => 'date',
+			'value'   => $dates,
+			'compare' => 'BETWEEN'
+		)
+	);
+
+	$meta_query[] = $mod_date_query;
+	return $meta_query;
+}
+add_filter( 'epl_preprocess_search_meta_query', 'my_epl_mod_date_custom_search_processing', 10, 2 );
