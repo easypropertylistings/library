9e3d66f483867b6f5a1a019c5f27b516cfa855a5
Merv Barrett <merv@realestateconnected.com.au>
2021-09-02 10:41:46 +0800

    Update shortcode-page-builder-loop.php

--- utilities/shortcode-page-builder-loop.php
+++ utilities/shortcode-page-builder-loop.php
@@ -1,20 +1,24 @@
 <?php
 /**
+ * Page Builder Archive / Search Results Loop
  * Load the Easy Property Listings Loop using a shortcode. For use in Page Buiders like Elementor, Divi, WP Bakery, Visual Composer.
  * Usage is [my_epl_search_results_loop]
+ *
+ * @since 1.0.0
+ * @since 1.0.1 Corrected issue when not used on an archive page.
  */
-
-// Page Builder Archive / Search Results Loop
 function my_epl_search_results_loop_callback() {
 
-	ob_start(); ?>
+	ob_start();
 
-	<?php if(have_posts()) : ?>
-		
+	if ( ( function_exists( 'epl_is_search' ) && true === epl_is_search() ) || ( function_exists( 'is_epl_core_post' ) && true === is_epl_core_post() ) ) {
+
+		if ( have_posts() ) : ?>
+
 		<div class="epl-template-blog">
 			<?php do_action( 'epl_property_loop_start' ); ?>
 
-			<?php while(have_posts()) : the_post(); ?>
+			<?php while( have_posts() ) : the_post(); ?>
 				<?php do_action( 'epl_property_blog' ); ?>
 			<?php endwhile; ?>
 
@@ -32,10 +36,10 @@ function my_epl_search_results_loop_callback() {
 
 			<?php do_action( 'epl_property_search_not_found' ); ?>
 
-		<?php endif; ?>
+		<?php endif;
+	}
 
-	<?php
-		return ob_get_clean();
+	return ob_get_clean();
 
 }
 add_shortcode( 'my_epl_search_results_loop', 'my_epl_search_results_loop_callback' );
