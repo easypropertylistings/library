47698b25500d65adcb292955c2aef2a4eb3a4c4a
Merv Barrett <merv@realestateconnected.com.au>
2022-04-25 14:16:55 +0800

    Update epl-custom-settings.php

--- custom-settings-plugin/epl-custom-settings.php
+++ custom-settings-plugin/epl-custom-settings.php
@@ -5,7 +5,7 @@
  * Description: Adds custom settings to Easy Property Listings
  * Version: 1.0.0
  * Author: Merv Barrett
- * Author URI: http://www.realestateconnected.com.au/
+ * Author URI: https://www.realestateconnected.com.au/
  */
 
-// Add your functions to this pluign. Install and activate it on your website.
+// Add your functions to this plugin. Install and activate it on your website.
