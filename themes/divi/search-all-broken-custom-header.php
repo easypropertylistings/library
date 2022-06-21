3ba21c9d717597b5b570d24d91bfa421155a6795
mervb1@gmail.com <mervb1@gmail.com>
2022-06-21 12:19:37 +0800

    Create search-all-broken-custom-header.php

---
+++ themes/divi/search-all-broken-custom-header.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Divi theme fix for a broken custom header/footer 
+ *
+ * Add this function to your child theme functions.php file
+ */
+function init_ds_fix_epl_search() {
+
+	if( isset( $_GET['tab_post_types'] ) && !isset( $_GET['post_type'] ) ) {
+		
+		$post_type = isset( $_GET['tab_post_types'] ) ? sanitize_text_field( $_GET['tab_post_types'] ) : '';
+		$post_type = explode(',', $post_type );
+		$post_type = array_filter( $post_type );
+
+		if( !empty( $post_type ) ) {
+			$_GET['post_type'] = $post_type;
+		}
+		
+	}
+}
+add_action( 'after_setup_theme', 'init_ds_fix_epl_search', 1);
