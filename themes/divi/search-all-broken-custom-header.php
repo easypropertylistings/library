78415c326df205fa349753591d41cfc77ac9e044
mervb1@gmail.com <mervb1@gmail.com>
2022-06-21 12:20:05 +0800

    Update search-all-broken-custom-header.php

--- themes/divi/search-all-broken-custom-header.php
+++ themes/divi/search-all-broken-custom-header.php
@@ -6,16 +6,16 @@
  */
 function init_ds_fix_epl_search() {
 
-	if( isset( $_GET['tab_post_types'] ) && !isset( $_GET['post_type'] ) ) {
-		
+	if ( isset( $_GET['tab_post_types'] ) && ! isset( $_GET['post_type'] ) ) {
+
 		$post_type = isset( $_GET['tab_post_types'] ) ? sanitize_text_field( $_GET['tab_post_types'] ) : '';
 		$post_type = explode(',', $post_type );
 		$post_type = array_filter( $post_type );
 
-		if( !empty( $post_type ) ) {
+		if( ! empty( $post_type ) ) {
 			$_GET['post_type'] = $post_type;
 		}
-		
+
 	}
 }
-add_action( 'after_setup_theme', 'init_ds_fix_epl_search', 1);
+add_action( 'after_setup_theme', 'init_ds_fix_epl_search', 1 );
