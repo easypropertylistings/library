b9199e0a3c83297fac3b84868a8c5264a9306177
mervb1@gmail.com <mervb1@gmail.com>
2022-06-22 13:24:08 +0800

    Update search-all-broken-custom-header.php

--- themes/divi/search-all-broken-custom-header.php
+++ themes/divi/search-all-broken-custom-header.php
@@ -3,19 +3,28 @@
  * Divi theme fix for a broken custom header/footer 
  *
  * Add this function to your child theme functions.php file
+ *
+ * @since 1.0.1 Support for all search possibilities.
  */
 function init_ds_fix_epl_search() {
-
-	if ( isset( $_GET['tab_post_types'] ) && ! isset( $_GET['post_type'] ) ) {
-
+ 
+	if ( isset( $_GET['tab_post_types'] ) && !isset( $_GET['post_type'] ) ) {
 		$post_type = isset( $_GET['tab_post_types'] ) ? sanitize_text_field( $_GET['tab_post_types'] ) : '';
 		$post_type = explode(',', $post_type );
 		$post_type = array_filter( $post_type );
-
-		if( ! empty( $post_type ) ) {
+		
+		if( !empty( $post_type ) ) {
 			$_GET['post_type'] = $post_type;
 		}
-
 	}
-}
-add_action( 'after_setup_theme', 'init_ds_fix_epl_search', 1 );
+	
+	if ( ( isset( $_GET['action'] ) && 'epl_search' === $_GET['action'] ) && !isset( $_GET['post_type'] ) && !isset( $_GET['tab_post_types'] ) ) {
+		$post_type = epl_get_active_post_types();
+		$post_type = array_keys( $post_type );
+		
+		if( !empty( $post_type ) ) {
+			$_GET['post_type'] = $post_type;
+		}
+	}
+ }
+ add_action( 'init', 'init_ds_fix_epl_search', 1 );
