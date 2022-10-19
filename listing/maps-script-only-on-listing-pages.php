5444de1b19cf5933959de5b4e04a8d55c2eedf03
Merv Barrett <merv@realestateconnected.com.au>
2022-10-19 13:18:56 +0800

    Update maps-script-only-on-listing-pages.php

--- listing/maps-script-only-on-listing-pages.php
+++ listing/maps-script-only-on-listing-pages.php
@@ -5,7 +5,7 @@
  */
 function my_epl_google_maps_script_only_on_single_listing() {
 	if ( function_exists( 'is_epl_post_single' ) ) {
-		if ( ! is_epl_post_single() ) {
+		if ( is_epl_post_single() ) {
 			wp_dequeue_script( 'google-map-v-3' );
 		}
 	}
