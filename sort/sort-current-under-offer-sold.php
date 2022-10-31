42016d725c68043196a70e0249f4fc98836298eb
mervb1@gmail.com <mervb1@gmail.com>
2022-10-31 11:16:49 +0800

    Update sort-current-under-offer-sold.php

--- sort/sort-current-under-offer-sold.php
+++ sort/sort-current-under-offer-sold.php
@@ -1,26 +1,57 @@
 <?php
+
 /**
- * Listing shortcode sorted in following order :
- - Current
- - Current ( Under Offer )
- - Sold
- * @param  [type] $query [description]
- * @return [type]        [description]
+ * Sort a specific shortcode by instance_id. 
+ *
+ * Sorting by Current, Under Offer, Sold order.
+ *
+ * E.g: [listing instance_id="sort_order"]
+ *
+ * @requires EPL 3.3+
  */
-function epl_alter_orderby($query) {
+function my_epl_sort_current_under_offer_sold( $query ) {
+
+	if ( $query->get( 'is_epl_shortcode' ) && 'sort_order' === $query->get( 'instance_id' ) ) { // Adjust sort_sold to be whatever you require.
+
+		$meta_query = $query->get( 'meta_query' );
+		$meta_query['property_status_clause'] = array(
+			'key'     => 'property_status',
+			'value'   => '',
+			'compare' => '!='
+		);
+		$query->set( 'meta_query', $meta_query);
+		$query->set( 'meta_key', 'property_under_offer');
+		$query->set( 'orderby', array( 'property_status_clause' => 'ASC', 'property_under_offer' => 'ASC' ) );
+
+	}
+
+}
+add_action( 'pre_get_posts', 'my_epl_sort_current_under_offer_sold' , 20  );
+
+/**
+ * Best is to use the instance_id function above as this will sort all listings by this order.
+ *
+ * - Current
+ * - Current ( Under Offer )
+ * - Sold
+ *
+ * @param  [type] $query The query.
+ */
+function my_epl_alter_orderby( $query ) {
 
 	if( !$query->is_main_query() && in_array('property', (array) $query->get('post_type') ) ) {
 
-		$meta_query = $query->get('meta_query');
+		$meta_query = $query->get( 'meta_query' );
 		$meta_query['property_status_clause'] = array(
-			'key'		=>	'property_status',
-			'value'		=>	'',
-			'compare'	=>	'!='
+			'key'     => 'property_status',
+			'value'   => '',
+			'compare' => '!='
 		);
-		$query->set('meta_query',$meta_query);
-		$query->set('meta_key','property_under_offer');
-		$query->set('orderby', array('property_status_clause' => 'ASC','property_under_offer' => 'ASC'));
+		$query->set( 'meta_query', $meta_query);
+		$query->set( 'meta_key', 'property_under_offer');
+		$query->set( 'orderby', array( 'property_status_clause' => 'ASC', 'property_under_offer' => 'ASC' ) );
 
 	}
 }
-add_action('pre_get_posts','epl_alter_orderby',99);
+add_action( 'pre_get_posts', 'my_epl_alter_orderby', 99 );
+
