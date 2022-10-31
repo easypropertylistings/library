d7816a34f19ae715fa144090e217e69eacc31aab
mervb1@gmail.com <mervb1@gmail.com>
2022-10-31 11:10:32 +0800

    Update sort-sold-listings.php

--- sort/sort-sold-listings.php
+++ sort/sort-sold-listings.php
@@ -4,54 +4,60 @@
  *
  */
 
-
-
 /**
- * Sort Sold Listings by Page slug: See version 2 below.
+ * Sort a specific shortcode by instance_id
  *
+ * E.g: [listing status=sold instance_id="sort_sold"]
+ *
+ * @requires EPL 3.3+
  */
-function my_epl_sort_recent_sales( $query ) {
+function my_epl_sort_recent_sales_home( $query ) {
 
-	// Do nothing if page is not recent sales page
-	if ( is_admin() || $query->is_main_query() || is_search() || !is_page('recent-sales') ) // Adjust the page slug
-		return;
+	if( $query->get('is_epl_shortcode') && $query->get('instance_id') == 'sort_sold' ) {
 
-	if( !in_array('property',(array) $query->query_vars['post_type']))
-		return;
+		$meta_query = $query->get('meta_query');
+		$meta_query[] = array(
+			'key'		=> 'property_sold_date',
+			'type'		=> 'DATE',
+			'compare'	=> 'EXISTS',
+		);
 
-	// Do nothing if Easy Property Listings is not active
-	if ( ! function_exists( 'epl_all_post_types' ) )
-		return;
+		$query->set('meta_query',$meta_query);
 
-	$meta_query = array(
+		$query->set('meta_key', 'property_sold_date' );
+		$query->set('orderby', array('meta_value' => 'DESC') );
 
-		'property_sold_date'	=>
-			array(
-				'key'		=> 'property_sold_date',
-				'value'		=> '',
-				'type'		=>	'DATE',
-				'compare'	=> '!=',
-			),
-	);
+	}
 
-	$query->set( 'posts_per_page' , 99 ); // Adjust the quantity
-	$query->set('meta_query',$meta_query);
-	$query->set( 'orderby',
-		array(
-			'property_sold_date' 	=> 'DESC'
-		)
-	);
 }
-add_action( 'pre_get_posts', 'my_epl_sort_recent_sales' , 1  );
-
+add_action( 'pre_get_posts', 'my_epl_sort_recent_sales_home' , 20  );
 
 /**
- * Sort Sold Listings: Version 2
+ * Display Sold Date: Good for testing.
+ *
+ */
+function my_epl_display_sold_date() {
+	global $property;
+
+	$sold_date = $property->get_property_price_sold_date( '' );
+
+	if ( $sold_date ) {
+		?>
+		<div class="sold-date">
+			<span><?php echo esc_html( $sold_date); ?></span>
+		</div>
+		<?php
+	}
+}
+add_action( 'epl_property_price', 'my_epl_display_sold_date' );
+
+/**
+ * Sort Sold Listings by page-slug. Using instance_id function above is a better option when sorting.
  *
  */
 function my_epl_sort_recent_sales( $query ) {
 	// Do nothing if page is not recent sales page
-	if ( is_admin() || $query->is_main_query() || is_search() || !is_page('recent-sales') ) { // Adjust the page slug.
+	if ( is_admin() || $query->is_main_query() || is_search() || ! is_page('recent-sales') ) { // Adjust the page slug.
 		return;
 	}
 
@@ -85,49 +91,4 @@ function my_epl_sort_recent_sales( $query ) {
 
 add_action( 'pre_get_posts', 'my_epl_sort_recent_sales' , 20  );
 
-/**
- * Display Sold Date: Good for testing.
- *
- */
-function my_epl_display_sold_date() {
-	global $property;
 
-	$sold_date = $property->get_property_price_sold_date( '' );
-
-	if ( $sold_date ) {
-		?>
-		<div class="sold-date">
-			<span><?php echo esc_html( $sold_date); ?></span>
-		</div>
-		<?php
-	}
-}
-add_action( 'epl_property_price', 'my_epl_display_sold_date' );
-
-/*
- * Sort a specific epl shortcode by instance_id
- *
- * E.g: [listing status=sold instance_id="sort_sold"]
- *
- * @requires EPL 3.3+
- */
-function my_epl_sort_recent_sales_home( $query ) {
-
-	if( $query->get('is_epl_shortcode') && $query->get('instance_id') == 'sort_sold' ) {
-
-		$meta_query = $query->get('meta_query');
-		$meta_query[] = array(
-			'key'		=> 'property_sold_date',
-			'type'		=> 'DATE',
-			'compare'	=> 'EXISTS',
-		);
-
-		$query->set('meta_query',$meta_query);
-
-		$query->set('meta_key', 'property_sold_date' );
-		$query->set('orderby', array('meta_value' => 'DESC') );
-
-	}
-
-}
-add_action( 'pre_get_posts', 'my_epl_sort_recent_sales_home' , 20  );
