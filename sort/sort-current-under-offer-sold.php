c3258ce4ad43773facf27b0dfc74678ac94d7bb6
Merv Barrett <mervb1@gmail.com>
2023-04-05 14:34:42 +0800

    Update sort-current-under-offer-sold.php

--- sort/sort-current-under-offer-sold.php
+++ sort/sort-current-under-offer-sold.php
@@ -1,17 +1,26 @@
 <?php
 
 /**
- * Sort a specific shortcode by instance_id. 
+ * Best is to use the instance_id function above as this will sort all listings by this order.
  *
- * Sorting by Current, Under Offer, Sold order. It's best to aadd the post type when using this sort.
+ * Usage: [listing instance_id="sort_current_under_offer_sold"]
  *
- * E.g: [listing post_type=property instance_id="sort_order"]
+ * - Current (published date first)
+ * - Current
+ * - Current
+ * - Current ( Under Offer ) (published date first)
+ * - Current ( Under Offer )
+ * - Current ( Under Offer )
+ * - Sold (published date first)
+ * - Sold
+ * - Sold
+ * - Sold
  *
- * @requires EPL 3.3+
+ * @param array $query The query.
  */
-function my_epl_sort_current_under_offer_sold( $query ) {
+function my_epl_alter_orderby( $query ) {
 
-	if ( $query->get( 'is_epl_shortcode' ) && 'sort_order' === $query->get( 'instance_id' ) ) { // Adjust sort_order to be whatever you require.
+	if ( $query->get( 'is_epl_shortcode' ) && 'sort_current_under_offer_sold' === $query->get( 'instance_id' ) )  {
 
 		$meta_query = $query->get( 'meta_query' );
 		$meta_query['property_status_clause'] = array(
@@ -21,15 +30,43 @@ function my_epl_sort_current_under_offer_sold( $query ) {
 		);
 		$query->set( 'meta_query', $meta_query);
 		$query->set( 'meta_key', 'property_under_offer');
-		$query->set( 'orderby', array( 'property_status_clause' => 'ASC', 'property_under_offer' => 'ASC' ) );
-
+		$query->set( 'orderby', 
+			array( 
+				'property_status_clause' => 'ASC',
+				'property_under_offer'   => 'ASC',
+				'date'                   => 'DESC' // Sort by published date.
+			) 
+		);
 	}
-
 }
-add_action( 'pre_get_posts', 'my_epl_sort_current_under_offer_sold' , 20  );
+add_action( 'pre_get_posts', 'my_epl_alter_orderby', 99 );
 
 /**
- * Best is to use the instance_id function above as this will sort all listings by this order.
+ * Display Published Date: Good for testing.
+ *
+ */
+function my_epl_display_published_date() {
+	global $property;
+
+	$date = $property->post->post_date;
+
+	if ( $date ) {
+		?>
+		<div class="published-date">
+			<span><?php echo esc_html( $date); ?></span>
+		</div>
+		<?php
+	}
+}
+add_action( 'epl_property_price', 'my_epl_display_published_date' );
+
+
+
+
+/**
+ * Sort search results property archive page.
+ *
+ * Best is to use the instance_id with a shortcode and the function above as this will sort all listings by this order.
  *
  * - Current
  * - Current ( Under Offer )
@@ -39,7 +76,7 @@ add_action( 'pre_get_posts', 'my_epl_sort_current_under_offer_sold' , 20  );
  */
 function my_epl_alter_orderby( $query ) {
 
-	if( !$query->is_main_query() && in_array('property', (array) $query->get('post_type') ) ) {
+	if( ! $query->is_main_query() && in_array('property', (array) $query->get('post_type') ) ) {
 
 		$meta_query = $query->get( 'meta_query' );
 		$meta_query['property_status_clause'] = array(
