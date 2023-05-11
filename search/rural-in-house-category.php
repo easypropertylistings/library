0831111073469a0db74b07808aa39d5cbf9f046e
Prince Singh <prince.kumar.cse@gmail.com>
2023-05-11 14:54:57 +0530

    Create rural-in-house-category.php

---
+++ search/rural-in-house-category.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Add the Land post type as a search House Category.
+ */
+
+// Add Rural option in Property Category.
+function my_epl_search_add_rural( $fields ) {
+	foreach($fields as $field_key   =>  &$field) {
+		if($field['meta_key'] == 'property_category') {
+			$field['options']['Rural'] = __('Rural','easy-property-listings');
+		}
+	}
+	return $fields;
+}
+add_filter( 'epl_search_widget_fields_frontend', 'my_epl_search_add_rural' );
+
+// Add script to select Rural Post type on selecting Category Rural.
+function my_epl_search_rural_script() { ?>
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			// On select change.
+			$('.epl-search-forms-wrapper #property_category').on('change', function(e) {
+		        if( $(this).find(':selected').val() == 'Rural' ) {
+		        	$(this).closest('form').find('#post_type').val('rural');
+		        } else {
+		        	$(this).closest('form').find('#post_type').val('property');
+		        }
+		    });
+		});
+	</script>
+<?php
+}
+add_action( 'wp_footer', 'my_epl_search_rural_script' );
+
+// Unset Property category if Land is selected.
+function my_epl_search_unset_property_category($query) {
+	$meta_query = $query->get('meta_query');
+
+	// Do nothing if in dashboard or not an archive page.
+	if (epl_is_search() && isset( $_GET['property_category'] ) && $_GET['property_category'] == 'Rural') {
+
+		if( !empty( $meta_query ) ) :
+		foreach($meta_query as $index => &$metakey) {
+			if($metakey['key'] == 'property_category') {
+				unset($meta_query[$index]);
+			}
+		}
+		endif;
+                //$post_types = (array) $query->get( 'post_type' );
+                $post_types = 'rural';
+                $query->set('post_type',$post_types);
+		$query->set('meta_query',$meta_query);
+	}
+}
+add_filter( 'pre_get_posts', 'my_epl_search_unset_property_category' );
+
+// Include rural in search results
+function my_epl_search_include_rural($query) {
+
+	$meta_query = $query->get('meta_query');
+
+	// Do nothing if in dashboard or not an archive page.
+	if ( epl_is_search() ) {
+		
+                $post_types = (array) $query->get( 'post_type' );
+                $post_types[] = 'rural';
+               
+                $query->set('post_type',$post_types);
+	}
+}
+add_filter( 'pre_get_posts', 'my_epl_search_include_rural', 99 );
