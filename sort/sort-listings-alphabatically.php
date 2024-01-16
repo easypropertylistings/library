85fd9301f4bc759d8bd8dc87d8867e972aab07ce
Merv Barrett <mervb1@gmail.com>
2024-01-16 16:08:20 +0800

    Updated sort by location a-z

--- sort/sort-listings-alphabatically.php
+++ sort/sort-listings-alphabatically.php
@@ -1,21 +1,22 @@
 <?php
 /**
- * Sort a specific epl shortcode by instance_id
+ * Sort location by a-z using instance_id option in shortcode.
  *
- * E.g: [listing instance_id="sort_alpha"]
+ * E.g: [listing instance_id="sort_location_alpha"]
  *
  * @requires EPL 3.3+
  */
-function my_epl_instance_id_sort_alpha_callback( $query ) {
+function my_epl_instance_id_sort_location_alpha_callback( $query ) {
 
 	// Do nothing if user selects a sorting options.
 	if ( isset ( $_GET['sortby'] ) ) {
 		return;
 	}
 
-	if ( $query->get( 'is_epl_shortcode' ) && 'sort_alpha' === $query->get( 'instance_id' ) ) {
-		$query->set( 'orderby', 'post_title' );
+	if ( $query->get( 'is_epl_shortcode' ) && 'sort_location_alpha' === $query->get( 'instance_id' ) ) {
+		$query->set( 'meta_key', 'property_address_suburb' );
+		$query->set( 'orderby', 'property_address_suburb' );
 		$query->set( 'order', 'ASC' );
 	}
 }
-add_action( 'pre_get_posts', 'my_epl_instance_id_sort_alpha_callback' , 20  );
+add_action( 'pre_get_posts', 'my_epl_instance_id_sort_location_alpha_callback' , 20  );
