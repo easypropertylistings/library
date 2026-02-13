5fc945f525db556fd007452763f7e5f8f7bf8d86
Merv Barrett <mervb1@gmail.com>
2026-02-13 16:05:52 +0800

    updated land and added building

--- listing/custom-building-icon.php
+++ listing/custom-building-icon.php
@@ -30,13 +30,24 @@ add_filter( 'epl_get_property_icons', 'my_epl_property_icons_add_building' );
  */
 function my_get_property_new_building( $returntype = 'i' ) {
 	
+	global $property;
+	
 	if ( empty( $returntype ) ) {
 		$returntype = 'i';
 	}
+	
+	$building_area = $property->get_property_meta( 'property_building_area' );
 
-	global $property;
 
-	if( 0 !== intval( $property->get_property_meta( 'property_building_area') ) ) {
+	if( 0 !== intval( $building_area ) ) {
+		
+		// Decimal.
+		if ( fmod( floatval( $building_area ), 1 ) !== 0.00 ) {
+			$building_area_format = apply_filters( 'epl_property_building_area_format_decimal', number_format_i18n( $building_area, 2 ) );
+		} else {
+			// No decimal.
+			$building_area_format = apply_filters( 'epl_property_building_area_format', number_format_i18n( $building_area ) );
+		}
 
 		$property_building_area_unit = $property->get_property_meta( 'property_building_area_unit' );
 
@@ -44,8 +55,8 @@ function my_get_property_new_building( $returntype = 'i' ) {
 			$property_building_area_unit = __('sqm' , 'epl');
 		}
 
-		$building['l'] = '<li class="building-size">'. __('building is', 'epl').' ' . $property->get_property_meta('property_building_area') .' '.$property_building_area_unit.'</li>';
-		$building['i'] = '<span title="'.__('building', 'epl').'" class="icon building"><span class="icon-value">' . $property->get_property_meta('property_building_area') .' '.$property_building_area_unit.'</span></span>';
+		$building['l'] = '<li class="building-size">'. __('building is', 'epl').' ' . $building_area_format .' '.$property_building_area_unit.'</li>';
+		$building['i'] = '<span title="'.__('building', 'epl').'" class="icon building"><span class="icon-value">' . $building_area_format .' '.$property_building_area_unit.'</span></span>';
 		
 		echo wp_kses_post( $building[$returntype] );
 	}
--- listing/custom-land-icon.php
+++ listing/custom-land-icon.php
@@ -2,7 +2,7 @@
 /**
  * Create a custom land icon
  *
-**/
+ */
 
 /**
  * Add new_land to Icons using epl_get_property_icons filter.
@@ -21,22 +21,33 @@ function my_epl_property_icons_add_land( $icons ) {
 }
 add_filter( 'epl_get_property_icons', 'my_epl_property_icons_add_land' );
 
+
 /**
  * Register new_land icon type hook to the dynamic epl_get_property_icon_NEW_HOOK_NAME_EG_new_land
  *
  * @param string $returntype Icon type.
  *
- * @return void  Echo the icon.
- */
+ * @return void  Echo the icon. 
+*/
 function my_get_property_new_land( $returntype = 'i' ) {
 	
+	global $property;
+
 	if ( empty( $returntype ) ) {
 		$returntype = 'i';
 	}
 
-	global $property;
+	$property_land_area = $property->get_property_meta( 'property_land_area' );
 
-	if( 0 !== intval( $property->get_property_meta( 'property_land_area') ) ) {
+	if ( 0 !== intval( $property_land_area ) ) {
+		
+		// Decimal.
+		if ( fmod( floatval( $property_land_area ), 1 ) !== 0.00 ) {
+			$property_land_area_format = apply_filters( 'epl_property_land_area_format_decimal', number_format_i18n( $property_land_area, 2 ) );
+		} else {
+			// No decimal.
+			$property_land_area_format = apply_filters( 'epl_property_land_area_format', number_format_i18n( $property_land_area ) );
+		}
 
 		$property_land_area_unit = $property->get_property_meta( 'property_land_area_unit' );
 
@@ -44,8 +55,8 @@ function my_get_property_new_land( $returntype = 'i' ) {
 			$property_land_area_unit = __('sqm' , 'epl');
 		}
 
-		$land['l'] = '<li class="land-size">'. __('Land is', 'epl').' ' . $property->get_property_meta('property_land_area') .' '.$property_land_area_unit.'</li>';
-		$land['i'] = '<span title="'.__('Land', 'epl').'" class="icon land"><span class="icon-value">' . $property->get_property_meta('property_land_area') .' '.$property_land_area_unit.'</span></span>';
+		$land['l'] = '<li class="land-size">'. __('Land is', 'epl').' ' . $property_land_area_format .' '.$property_land_area_unit.'</li>';
+		$land['i'] = '<span title="'.__('Land', 'epl').'" class="icon land"><span class="icon-value">' . $property_land_area_format .' '.$property_land_area_unit.'</span></span>';
 		
 		echo wp_kses_post( $land[$returntype] );
 	}
